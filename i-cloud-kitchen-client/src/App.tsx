77e81fe0d5986ef743255144c284d1e071f7ab6b
Dominic-SR <dominicseduraja3@gmail.com>
2026-04-20 23:12:49 +0530

    client setup

---
+++ i-cloud-kitchen-client/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+     <h1 className="text-3xl font-bold underline bg-red-500 text-white p-4  ">
+    Hello world!
+  </h1>
+  )
+}
+
+export default App
