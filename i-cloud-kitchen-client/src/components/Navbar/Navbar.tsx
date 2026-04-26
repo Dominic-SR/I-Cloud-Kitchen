f45f492df5006ff4fec63b6d65c2bb054403b269
Dominic-SR <dominicseduraja3@gmail.com>
2026-04-27 00:00:17 +0530

    nav setup

---
+++ i-cloud-kitchen-client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-[#2D1b0b] border-b-8 border-amber-900/30 shadow-amber-900/30 sticky top-0 z-50 shadow-[0_25px_50px_-12px] font-vibes group/nav overflow-x-hidden'>Navbar</div>
+  )
+}
+
+export default Navbar
