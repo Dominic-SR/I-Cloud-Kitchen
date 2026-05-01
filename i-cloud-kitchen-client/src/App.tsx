0f18ce0843442d1c975bbbfb37222b661d36a847
Dominic-SR <dominicseduraja3@gmail.com>
2026-05-01 23:39:52 +0530

    rounded nav button changes

--- i-cloud-kitchen-client/src/App.tsx
+++ i-cloud-kitchen-client/src/App.tsx
@@ -1,12 +1,18 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import './App.css'
+import About from './pages/About/About'
+import Contact from './pages/Contact/Contact'
+import Menu from './pages/Menu/Menu'
 
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/menu' element={<Menu />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/contact' element={<Contact />} /> 
     </Routes>
   )
 }
